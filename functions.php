b89fd150fa1f8ac9e0933db8b5a8821b3c83b339
merchans <addmarkovic@gmail.com>
2020-08-08 18:49:08 +0200

    pridal som vzorove CPT

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ include( get_theme_file_path( "/includes/custom-nav-walker.php" ) );
 include( get_theme_file_path( "/includes/widgets.php" ) );
 include( get_theme_file_path( "/includes/cleanup-and-reorder/cleanup.php" ) );
 include( get_theme_file_path( "/includes/cleanup-and-reorder/reorder-menu.php" ) );
+include( get_theme_file_path( "/includes/custom-posts-types/cpt-1.php" ) );
 
 
 // Hooks
@@ -18,6 +19,10 @@ add_action( "wp_enqueue_scripts", "mr_enqueue" );
 add_action( "after_setup_theme", "mr_setu_theme");
 add_action( "widgets_init", "mr_widgets");
 
+// Hooking up our function to theme setup
+add_action( 'init', 'mr_create_posttype' );
+// custom post types
+add_action('init', 'mr_post_type_news');
 
 add_action('init', 'mr_waste_scripts', PHP_INT_MAX - 1);
 
